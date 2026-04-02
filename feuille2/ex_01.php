ada5e48a9dc3ff55d365c44e76b5dcc13acd1ffb
Jonas Dohou <jonasdohou.dev@gmail.com>
2026-04-02 18:41:40 +0100

    ajout feulle2

---
+++ feuille2/ex_01.php
@@ -0,0 +1,17 @@
+<?php
+
+// une fonction « my_included_putstr » qui affichera la chaîne de caractères fournie en paramètre.
+function my_included_putstr(string $str) : void {
+    echo $str . "\n";
+}
+
+
+// incluez ex_01_2.php et appelez la fonction « my_included_putstr » en lui fournissant le premier argument depuis la ligne de commande.
+
+include "ex_01_2.php";
+
+if (isset($argv[1])) {
+    
+    my_included_putstr($argv[1]);
+}
+?>
