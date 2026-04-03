4ce92cfc610d602de2c72e44635d3bbdbacf2c7b
Jonas Dohou <jonasdohou.dev@gmail.com>
2026-04-03 20:11:18 +0100

    modif ex_05

--- feuille2/ex_05.php
+++ feuille2/ex_05.php
@@ -2,8 +2,8 @@
 
 
 
-function my_add_elem_map($key, $value, &$map)
-{
+function my_add_elem_map(mixed $key, mixed $value, mixed &$map){
+
     if (!is_array($map)) {
         echo "Vous devez fournir des paramètres corrects.\n";
         return NULL;
@@ -13,34 +13,45 @@ function my_add_elem_map($key, $value, &$map)
     return $map;
 }
 
-function my_modify_elem_map($key, $value, &$map)
-{
+function my_modify_elem_map(mixed $key, mixed $value, mixed &$map) {
+
     if (!is_array($map) || !array_key_exists($key, $map)) {
         echo "Vous devez fournir des paramètres corrects.\n";
-        return NULL;
+
+        return is_array($map) ?  
+        $map : [];
     }
 
     $map[$key] = $value;
     return $map;
 }
 
-function my_delete_elem_map($key, &$map)
-{
-    if (!is_array($map) || !array_key_exists($key, $map)) {
+function my_delete_elem_map(mixed $key, mixed &$map) {
+
+    if (!is_array($map) || is_null($key) || !array_key_exists($key, $map)) {
         echo "Vous devez fournir des paramètres corrects.\n";
-        return NULL;
+        return is_array($map) ? $map : [];
     }
 
     unset($map[$key]);
     return $map;
 }
 
-function my_is_elem_valid($key, $value, &$map)
-{
-    if (!is_array($map) || !array_key_exists($key, $map)) {
-        echo "Vous devez fournir des paramètres corrects.\n";
+function my_is_elem_valid(mixed $key, mixed $value, mixed &$map) : bool {
+
+    if (!is_array($map) || is_null($key) || !array_key_exists($key, $map)) {
+        echo "Vous devez fournir des paramètres corrects.\n"; 
         return false;
     }
+
+    if(!array_key_exists($key, $map) || $map[$key] !== $value) {
+
+        echo "La clé n'existe pas dans la map.\n";
+        return false;
+
+    } else {
+        echo "La clé existe dans la map et sa valeur correspond à la valeur fournie.\n";
+    }   return true;
 
     return $map[$key] === $value;
 }
