2fd35f57530741dd00ebfcfa2297b1a8ae913e30
Jonas Dohou <jonasdohou.dev@gmail.com>
2026-05-13 17:56:24 +0100

    corection

--- feuille6/ex_05/IUnit.php
+++ feuille6/ex_05/IUnit.php
@@ -2,20 +2,13 @@
 
 interface IUnit {
     public function getName(): string;
-    
     public function getPV(): int;
-
     public function getPA(): int;
+    public function getDamage(): int;
 
     public function equip(): void;
-
     public function attack(IUnit $target): void;
-
     public function receiveDamage(int $damage): void;
-
     public function moveCloseTo(IUnit $target): void;
-
     public function recoverAP(): void;
-
-
 }
